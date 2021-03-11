0b6385fe263e60309bcd03a56740f6598f6a6ce4
adhie1337 <adhie@inf.elte.hu>
2021-03-11 19:30:06 +0100

    Update Scorer

--- src/TrafficLights.Console/Scorer.cs
+++ src/TrafficLights.Console/Scorer.cs
@@ -2,125 +2,118 @@ namespace TrafficLights.Console
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
     using TrafficLights.Common;
 
     public static class Scorer
     {
+        public static Schedule WithScore(Input input, Schedule schedule)
+        {
+            if (schedule.Score.HasValue) return schedule;
+
+            var score = Score(input, schedule);
+
+            return new Schedule(schedule.Get, score);
+        }
+
         public static int Score(Input input, Schedule schedule)
         {
+            if (schedule.Score.HasValue) return schedule.Score.Value;
+
             var remainingTime = input.Duration + 1;
             var time = 0;
             var score = 0;
             var arrivedCars = new HashSet<int>();
-            var carIdQueue = Enumerable.Range(0, input.StreetCount).Select(strId => new Queue<int>(input.Cars.Where(_ => _.Streets[0].Id == strId).Select(_ => _.Id))).ToArray();
-            var remainingTimeOnStreet = input.Cars.Select(_ => _.Streets[0].Time).ToArray();
-            var carStreetIndexes = Enumerable.Repeat(0, input.CarCount).ToArray();
-            var schedules = schedule.Get.ToDictionary
-            (
-                _ => _.Intersection.Id,
-                _ => _.Streets
-                    .ScanM(new { Id = 0, Wait = 0, Time = 0 }, (acc, st) => new { st.Street.Id, Wait = acc.Wait + acc.Time, st.Time })
-                    .ToDictionary(_ => _.Id, _ => (_.Wait, _.Time))
-            );
-            var scheduleCycles = schedules.ToDictionary(_ => _.Key, _ => _.Value.Values.Sum(_ => _.Time));
+            var carIdQueue = new int?[input.StreetCount];
+            var remainingTimeOnStreet = new int[input.CarCount];
+            for (var i = 0; i < input.CarCount; ++i) remainingTimeOnStreet[i] = input.Cars[i].Streets[0].Time;
+            var carStreetIndexes = new int[input.CarCount];
+            var schedules = new Dictionary<int, (int Wait, int Time)>[input.IntersectionCount];
+            var scheduleCycles = new int[input.IntersectionCount];
+
+            foreach (var sch in schedule.Get)
+            {
+                var d = new Dictionary<int, (int Wait, int Time)>();
+                var wait = 0;
+
+                for (var i = 0; i < sch.Streets.Length; ++i)
+                {
+                    var s = sch.Streets[i];
+                    d[s.Street.Id] = (wait, s.Time);
+                    wait += s.Time;
+                }
+
+                scheduleCycles[sch.Intersection.Id] = wait;
+
+                schedules[sch.Intersection.Id] = d;
+            }
+
             var intersectionsDone = new HashSet<int>();
-
-            //Console.WriteLine(string.Join("\n\n", schedules.Select(_ => $"Intersection {_.Key} schedule:\n - " + string.Join("\n - ", _.Value.Select(_ => $"Street: {_.Key}, wait: {_.Value.Wait}, time: {_.Value.Time}")))));
-
-            //Console.WriteLine("cars:\n - " + string.Join("\n - ", input.Cars.Select(c => c.Id + "\n  - " + string.Join("\n  - ", c.Streets.Select(_ => $"{_.Id} -> {_.End} in {_.Time}")))));
 
             while (remainingTime-- > 0)
             {
                 intersectionsDone.Clear();
 
-                foreach (var car in input.Cars)
+                for (var i = 0; i < input.Cars.Length; ++i)
                 {
+                    var car = input.Cars[i];
+
                     if (arrivedCars.Contains(car.Id)) continue;
 
                     var streetIndex = carStreetIndexes[car.Id];
                     var streetId = car.Streets[streetIndex].Id;
 
-                    var remaining = remainingTimeOnStreet[car.Id] = Math.Max(0, remainingTimeOnStreet[car.Id] - 1);
+                    var remaining = remainingTimeOnStreet[car.Id] - 1;
+                    if (remaining < 0) remaining = 0;
+                    remainingTimeOnStreet[car.Id] = remaining;
 
-                    //Console.WriteLine($"Car {car.Id}, remaining time: {remaining}");
-
-                    if (remaining == 0 && streetIndex < car.PathLength - 1)
+                    if (remaining == 0
+                        && streetIndex < car.PathLength - 1
+                        && !carIdQueue[streetId].HasValue)
                     {
-                        if (!carIdQueue[streetId].Contains(car.Id)) carIdQueue[streetId].Enqueue(car.Id);
-                        //Console.WriteLine($"Car {car.Id}, is at the end of: {streetId}");
+                        carIdQueue[streetId] = car.Id;
                     }
 
-                    if (remaining != 0)
-                    {
-                        //Console.WriteLine($"Skip car {car.Id} because it has remaining time on the road: {remaining}\n");
-                        continue;
-                    }
+                    if (remaining != 0) continue;
 
                     if (streetIndex == car.PathLength - 1)
                     {
-                        //Console.WriteLine($"Car {car.Id} arrived! Reward: {input.Reward} remaining time: {remainingTime}\n");
                         arrivedCars.Add(car.Id);
                         score += (input.Reward + remainingTime);
                         continue;
                     }
                     else if (remainingTime == 0)
                     {
-                        //Console.WriteLine("Skipping because there is no time left.\n");
                         continue;
                     }
 
-                    var queue = carIdQueue[streetId];
-                    if (queue.Count == 0 || queue.Peek() != car.Id)
-                    {
-                        //Console.WriteLine("Skipping because car is not the first in line.\n");
-                        continue;
-                    }
+                    if (carIdQueue[streetId] != car.Id) continue;
+
                     var intersectionId = car.Intersections[streetIndex].IntersectionId;
-                    if (intersectionsDone.Contains(intersectionId))
-                    {
-                        //Console.WriteLine("Skipping because intersection is already done.\n");
-                        continue;
-                    }
 
-                    var s = schedules[intersectionId][streetId];
+                    if (intersectionsDone.Contains(intersectionId)) continue;
 
-                    var timePart = scheduleCycles.TryGetValue(intersectionId, out var r) ? time % r : -1;
+                    var cycle = scheduleCycles[intersectionId];
+                    var timePart = cycle > 0 ? time % cycle : -1;
 
-                    if (timePart == -1 || timePart < s.Wait || timePart >= s.Wait + s.Time)
-                    {
-                        //Console.WriteLine("Skipping because of bad timeframe.\n");
-                        continue;
-                    }
+                    if (timePart == -1) continue;
+
+                    var s = schedules[intersectionId] != null && schedules[intersectionId].ContainsKey(streetId)
+                        ? schedules[intersectionId][streetId]
+                        : null as (int Wait, int Time)?;
+
+                    if (!s.HasValue || timePart < s.Value.Wait || timePart >= s.Value.Wait + s.Value.Time) continue;
 
                     intersectionsDone.Add(intersectionId);
-                    queue.Dequeue();
+                    carIdQueue[streetId] = null;
                     carStreetIndexes[car.Id] = streetIndex + 1;
                     var newStreet = car.Streets[streetIndex + 1];
                     remainingTimeOnStreet[car.Id] = newStreet.Time;
-
-                    //Console.WriteLine($"Car {car.Id} goes through intersection {intersectionId} to {newStreet.Id} for {newStreet.Time}\n");
                 }
-
-                var qs = carIdQueue.Select((q, i) => $"[{ i }: { string.Join(", ", q) }]");
-                //Console.WriteLine($"Queues: {string.Join(" ", qs)}\n");
 
                 time++;
             }
 
             return score;
-        }
-    }
-
-    public static class IEnumerableExtensions
-    {
-        public static IEnumerable<U> ScanM<T, U>(this IEnumerable<T> input, U state, Func<U, T, U> next)
-        {
-            foreach(var item in input)
-            {
-                state = next(state, item);
-                yield return state;
-            }
         }
     }
 }
